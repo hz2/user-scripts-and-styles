53e9d5aae7e9327d51d0fb03ecd838d30a33339d
Hz² <710921+hz2@users.noreply.github.com>
2021-08-06 18:01:37 +0800

    Update download-original-picture.js

--- scripts/download-original-picture.js
+++ scripts/download-original-picture.js
@@ -64,6 +64,35 @@ head[0].insertAdjacentHTML('beforeend', `<style type="text/css">
 </style>`);
 
 
+console.warn('Welcome to %c \ud83d\ude48\ud83d\ude49\ud83d\ude4a\u0020\u0048\u007a\u00b2\u0020\u0053\u0063\u0072\u0069\u0070\u0074\u0020\u004c\u0069\u0062\u0072\u0061\u0072\u0079 %c v0.06 ', 'background-color:teal;color: white;border:1px solid teal;border-radius: 4px 0 0 4px;border-left-width:0;padding:1px;margin:2px 0;font-size:1.1em', 'background-color:#777;color: white;border:1px solid #777;border-radius: 0 4px 4px 0;border-right-width:0;padding:1px;margin:5px 0;');
+ 
+const openDown = (url, e, name) => {
+  e && e.preventDefault();
+  e && e.stopPropagation()
+  fetch(url, {
+      mode: "cors"
+    })
+    .then(resp => resp.blob())
+    .then(r => {
+      const blobUrl = URL.createObjectURL(r)
+      let el = document.createElement("a");
+      el.setAttribute("href", blobUrl);
+      if (name) {
+        el.setAttribute("download", name)
+      }
+      if (document.createEvent) {
+        const event = document.createEvent("MouseEvents");
+        event.initEvent("click", true, true);
+        el.dispatchEvent(event);
+      } else {
+        el.click();
+      }
+    })
+    .catch(err => {
+      console.log("Request failed", err);
+    });
+}
+
 const hostname = window.location.hostname
 
 const lastItem = arr => arr.length ? arr[arr.length - 1] : ''
