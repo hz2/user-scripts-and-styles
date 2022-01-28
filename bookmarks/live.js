4172a6e41ca96a7f8e6a6bb8fa78282835b670fc
Hz2 <710921+hz2@users.noreply.github.com>
2022-01-28 23:16:39 +0800

    Update live.js

--- bookmarks/live.js
+++ bookmarks/live.js
@@ -8,6 +8,16 @@ javascript:(()=>{
             left: 0;
             z-index: 999; `);
       document.querySelector('.player-float').setAttribute('style', 'filter: none;');
+    };
+    if (host === 'live.bilibili.com') {
+      document.querySelector('.player-section').setAttribute('style', `width: 100vw;
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: 99999; `);
+      document.querySelector('#link-navbar-vm').setAttribute('style', 'display: none;');
+      document.querySelector('#aside-area-vm').setAttribute('style', 'display: none;');
     }
 })()
 
