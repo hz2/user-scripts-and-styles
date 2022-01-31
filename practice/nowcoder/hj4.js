84f552e28a908214d68ae2c931dc2e5f8b1d58a9
huc <ht@live.se>
2022-02-01 00:01:50 +0800

    update

--- practice/nowcoder/hj4.js
+++ practice/nowcoder/hj4.js
@@ -6,3 +6,13 @@ while (line = readline()) {
   }
   console.log(word.padEnd(8, 0))
 }
+
+
+// solution 2
+
+while (line = readline()) {
+  let arr = line.split('').concat([0, 0, 0, 0, 0, 0, 0])
+  while (arr.length >= 8) {
+    print(arr.splice(0, 8).join(''))
+  }
+}
