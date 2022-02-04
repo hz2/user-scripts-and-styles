0cf9039ab8577fba78391f0c4c916ffd8506d10c
huc <ht@live.se>
2022-02-05 00:26:04 +0800

    fix err

--- practice/nowcoder/hj39.js
+++ practice/nowcoder/hj39.js
@@ -4,11 +4,14 @@ const getNetwork = (ip, mask) => {
   return arr_ip.map((x, i) => x & arr_mask[i]).join('.')
 }
 
+const validIp = (mask, ip1, ip2) => [mask, ip1, ip2].every(x => /^((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}$/.test(x))
+const validMask = (mask) => mask.startsWith('255.') && mask.endsWith('.0')
+
 
 while (mask = readline()) {
   const ip1 = readline()
   const ip2 = readline()
-  if ([mask, ip1, ip2].some(x => !/^((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}$/.test(x))) {
+  if (!validIp(mask, ip1, ip2) || !validMask(mask)) {
     print(1);
   } else if (getNetwork(ip1, mask) === getNetwork(ip2, mask)) {
     print(0)
