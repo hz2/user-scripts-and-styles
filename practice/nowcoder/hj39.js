34055167d70e811a46c4d54458060a2682f5bcb1
huc <ht@live.se>
2022-02-05 00:12:42 +0800

    fix

--- practice/nowcoder/hj39.js
+++ practice/nowcoder/hj39.js
@@ -1,14 +1,14 @@
 const getNetwork = (ip, mask) => {
   const arr_ip = ip.split('.')
   const arr_mask = mask.split('.')
-  return arr_ip.map((x, i) => x && arr_mask[i]).join('.')
+  return arr_ip.map((x, i) => x & arr_mask[i]).join('.')
 }
 
 
 while (mask = readline()) {
   const ip1 = readline()
   const ip2 = readline()
-  if ([mask, ip1, ip2].some(x => !/^([0-2]?[0-5]?[0-5]\.){3}[0-2]?[0-5]?[0-5]$/.test(x))) {
+  if ([mask, ip1, ip2].some(x => !/^((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}$/.test(x))) {
     print(1);
   } else if (getNetwork(ip1, mask) === getNetwork(ip2, mask)) {
     print(0)
