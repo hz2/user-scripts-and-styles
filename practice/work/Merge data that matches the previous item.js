dd589671235edd52b927aedbdbfd23ec9e47c2a3
hz2 <710921+hz2@users.noreply.github.com>
2023-01-03 09:48:44 +0800

    Update Merge data that matches the previous item.js

--- practice/work/Merge data that matches the previous item.js
+++ practice/work/Merge data that matches the previous item.js
@@ -155,3 +155,108 @@ const mergeList = (arrold) => {
 //         ]
 //     }
 // ]
+
+
+const mergeListV2 = (arrold) => {
+  const arr = arrold.map(x => ({ ...x }))
+  for (const index in arr) {
+    let item = arr[index]
+    const lastItem = arr[index - 1]
+    if (item.countersign === "1") {
+      if (item.cj === lastItem.cj) {
+        arr[index] = { bf: [...lastItem.bf || [{ ...lastItem }], { ...item }], ...item };
+        arr[index - 1] = null
+      } else {
+        arr[index] = { ...(item || {}), bf: [item] }
+      }
+    }
+  }
+  return arr.filter(Boolean).map(({ bf, ...x }) => (bf ? { bf } : x))
+}
+
+// targer V2
+// [
+//   {
+//     "countersign": "0",
+//     "cj": 0
+//   },
+//   {
+//     "countersign": "0",
+//     "cj": 1
+//   },
+//   {
+//     "countersign": "0",
+//     "cj": 2
+//   },
+//   {
+//     "countersign": "0",
+//     "cj": 3
+//   },
+//   {
+//     "countersign": "0",
+//     "cj": 3
+//   },
+//   {
+//     "bf": [
+//       {
+//         "countersign": "1",
+//         "cj": 4
+//       },
+//       {
+//         "countersign": "1",
+//         "cj": 4
+//       }
+//     ]
+//   },
+//   {
+//     "bf": [
+//       {
+//         "countersign": "1",
+//         "cj": 5
+//       }
+//     ]
+//   },
+//   {
+//     "bf": [
+//       {
+//         "countersign": "1",
+//         "cj": 4
+//       },
+//       {
+//         "countersign": "1",
+//         "cj": 4
+//       }
+//     ]
+//   },
+//   {
+//     "bf": [
+//       {
+//         "countersign": "1",
+//         "cj": 5
+//       },
+//       {
+//         "countersign": "1",
+//         "cj": 5
+//       }
+//     ]
+//   },
+//   {
+//     "bf": [
+//       {
+//         "countersign": "1",
+//         "cj": 3
+//       },
+//       {
+//         "countersign": "1",
+//         "cj": 3
+//       },
+//       {
+//         "countersign": "1",
+//         "cj": 3
+//       }
+//     ]
+//   }
+// ]
+
+
+
