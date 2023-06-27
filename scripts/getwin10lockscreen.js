19bbfe333372ef6ef76f8ee10022054236054fa8
huc <ht@live.se>
2023-06-27 16:33:53 +0800

    add script

---
+++ scripts/getwin10lockscreen.js
@@ -0,0 +1,60 @@
+﻿const fs = require('fs');
+const os = require('os');
+const { exec } = require('child_process');
+
+// 环境变量与对应的路径
+// %ALLUSERSPROFILE%                 C:\ProgramData
+// %APPDATA%                              C:\Users\用户名\AppData\Roaming
+// %COMMONPROGRAMFILES%     C:\Program Files\Common Files
+// %COMMONPROGRAMFILES(x86)%   C:\Program Files (x86)\Common Files
+// %COMSPEC%                              C:\Windows\System32\cmd.exe
+// %HOMEDRIVE%和%SystemDrive%      C:\
+// %HOMEPATH%                           C:\Users\用户名
+// %LOCALAPPDATA%                    C:\Users\用户名\AppData\Local
+// %PROGRAMDATA%                    C:\ProgramData
+// %PROGRAMFILES%                     C:\Program Files
+// %PROGRAMFILES(X86)%             C:\Program Files (x86)
+// %PUBLIC%                                 C:\UsersPublic
+// %SystemRoot%                          C:\Windows
+// %TEMP%和%TMP%                    C:\Users\用户名\AppData\LocalTemp
+// %USERPROFILE%                        C:\Users用户名
+// %WINDIR%                                C:\Window
+
+
+const dir = `${os.homedir()}\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets`
+const outdir = process.env.OneDrive + '\\图片'
+
+
+
+// 新建输出目录；
+
+fs.readdir(outdir, (err, data) => {
+    if (err) throw err;
+    // console.log(err);
+    // console.log(data);
+    if (data.indexOf('BingLockScreen') == -1) {
+        fs.mkdirSync(`${outdir}/BingLockScreen`, (err, folder) => {
+            if (err) throw err;
+            console.log(folder);
+        });
+    } else {
+        console.log('输出目录已存在！');
+    }
+})
+
+fs.readdir(dir, (err, data) => {
+    if (err) throw err;
+    //    console.log(data);
+    data.map(x => {
+        fs.readFile(`${dir}/${x}`, (err, d) => {
+            //    console.log(d.length);
+            if (d.length > 1e5) {
+                fs.writeFile(`${outdir}/BingLockScreen/${x}.jpg`, d, (err) => {
+                    if (err) throw err;
+                    console.log(x + '.jpg 保存成功!');
+                });
+            }
+        })
+    })
+	exec('explorer %OneDrive%\\图片\\BingLockScreen')
+});
---
+++ scripts/redownloadBingWallpaper.js
@@ -0,0 +1,120 @@
+﻿const fs = require('fs');
+const path = require('path');
+os = require('os');
+const {
+    exec
+} = require('child_process');
+
+// node 18+
+
+const [nodepath, jspath, workdir] = process.argv
+
+const trytoMkdir = (dir) => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, {
+            recursive: true
+        });
+    }
+}
+
+
+function getReadableFileSizeString(fileSizeInBytes) {
+    var i = -1;
+    var byteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+    do {
+        fileSizeInBytes /= 1024;
+        i++;
+    } while (fileSizeInBytes > 1024);
+
+    return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+}
+
+const getKey = str => str.match(/OHR.\w+\_\w+-[A-Z\d]+/g)
+
+
+const openText = (objList) => {
+    const header = `\nOld Files Count: ${ objList.length } \nNew Files Count: ${  objList.filter(x=>!x.exist).length  }\n`
+    const c = header + `\n                  Old Files                        |      Size      |          Target Files      \n---------------------------------------------------|----------------|----------------------------------------------\n`
+    const text = path.join(workdir, "/RD/info.txt");
+    const data = c + objList.map(x => `${x.old.padEnd(50,' ')} |     ${ String( isNaN(x.size) ?  '-': getReadableFileSizeString(x.size) ).padEnd(8,' ') }   |   ${x.new || x.target }`).join('\n')
+    fs.writeFile(text, data, (err) => {
+        if (err) throw err;
+        exec(text)
+    });
+}
+const downImg = keyList => setTimeout(() => {
+    const objList = keyList.map(x => ({
+        old: x,
+        key: getKey(x)
+    }))
+    const newPath = file => path.join(workdir, "RD", "new", file)
+    keyList.forEach(async (x, i) => {
+        const filename = getKey(x) + `_UHD.jpg`
+        if (x.includes('_UHD.jpg')) {
+            objList[i].new = filename;
+            // objList[i].size = 'skip';
+            objList[i].finish = true;
+            console.log(`直接移动 ${filename}`);
+            fs.readFileSync(path.join(workdir, 'RD', 'old', filename), (err, d) => {
+                objList[i].size = d.length
+                fs.writeFileSync(newPath(filename), d, (err) => {
+                    if (err) throw err;
+                    console.log(`${filename} 保存成功!`);
+                });
+            })
+            if (objList.every(x => x.finish)) {
+                openText(objList)
+            }
+            return
+        }
+        console.log(`正在下载 ${filename}`);
+        const resp = await fetch(`https://www.bing.com/th?id=${filename}`)
+        const blob = await resp.blob()
+        const existIndex = objList.findIndex(y => y.size === blob.size)
+        if (existIndex !== -1) {
+            Object.assign(objList[i], {
+                exist: true,
+                target: objList[existIndex].new,
+                finish: true
+            })
+            if (objList.every(x => x.finish)) {
+                openText(objList)
+            }
+            return
+        }
+        objList[i].new = filename;
+        objList[i].size = blob.size;
+        const buffer = await blob.arrayBuffer()
+        objList[i].finish = true;
+        fs.writeFileSync(newPath(filename), Buffer.from(buffer), (err) => {
+            if (err) throw err;
+            console.log(`${filename} 保存成功!`);
+        });
+        if (objList.every(x => x.finish)) {
+            openText(objList)
+        }
+    })
+}, 120)
+
+
+fs.readdir(workdir, (err, data) => {
+    if (err) throw err;
+    trytoMkdir(`${workdir}/RD`);
+    trytoMkdir(`${workdir}/RD/old`);
+    trytoMkdir(`${workdir}/RD/new`);
+    data.forEach(x => {
+        const key = getKey(x);
+        if (key) {
+            fs.renameSync(path.join(workdir, x), path.join(workdir, 'RD', 'old', x), function (err) {
+                if (err) throw err
+                console.log('Successfully moved!')
+            })
+        }
+    })
+    setTimeout(() => {
+        fs.readdir(path.join(workdir, 'RD', 'old'), (err, data) => {
+            if (err) throw err;
+            downImg(data)
+        })
+    }, 1000)
+})
